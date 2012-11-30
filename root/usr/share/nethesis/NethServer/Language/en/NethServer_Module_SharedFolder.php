af7914353971310b843b162c5ea2f631efaa9272
Giacomo Sanchietti <giacomo.sanchietti@nethesis.it>
2012-11-30 08:45:16 +0100

    Translation: refactor english and italian translation files #523

--- root/usr/share/nethesis/NethServer/Language/en/NethServer_Module_SharedFolder.php
+++ root/usr/share/nethesis/NethServer/Language/en/NethServer_Module_SharedFolder.php
@@ -1,27 +1,23 @@
-<?php
-/**
- */
+<?php 
 
-$L['SharedFolder_Title'] = 'Shared Folders';
-$L['SharedFolder_Description'] = 'Share folders and files between users and groups';
-
-$L['Create_Header'] = 'Create a new shared folder';
-$L['Update_Header'] = 'Edit shared folder `${0}`';
-
-$L['BaseInfo_Title'] = 'General';
-$L['Acl_Title'] = 'ACL';
-$L['ibay_label'] = 'Name';
-$L['Key_label'] = 'Name';
-$L['OwningGroup_label'] = 'Owning group';
-$L['GroupAccess_label'] = 'Allow write permission to owning group';
-$L['OtherAccess_label'] = 'Allow read permission to everyone';
-$L['AclRead_label'] = 'Read';
-$L['AclWrite_label'] = 'Write';
-$L['Authenticated users'] = 'Authenticated users';
-
-$L['valid_platform,ibay-create,uniq-account,3'] = 'An account with the same name already exists';
-$L['valid_platform,ibay-create,ibay-length,1'] = 'Name too long';
-
-$L['reset-permissions_label'] = 'Reset permissions';
-$L['reset-permissions_Header'] = 'Reset permissions on folder `${0}`';
-$L['reset-permissions_Message'] = 'Reset permissions to default value on files and sub-folder of shared folder `${0}`?';
+$L['AclRead_label'] = 'Read'; 
+$L['AclWrite_label'] = 'Write'; 
+$L['Acl_Title'] = 'ACL'; 
+$L['Authenticated users'] = 'Authenticated users'; 
+$L['BaseInfo_Title'] = 'General'; 
+$L['Create_Header'] = 'Create a new shared folder'; 
+$L['GroupAccess_label'] = 'Allow write permission to owning group'; 
+$L['Key_label'] = 'Name'; 
+$L['OtherAccess_label'] = 'Allow read permission to everyone'; 
+$L['OwningGroup_label'] = 'Owning group'; 
+$L['SharedFolder_Description'] = 'Share folders and files between users and groups'; 
+$L['SharedFolder_Tags'] = 'folder directory shared ibay'; 
+$L['SharedFolder_Title'] = 'Shared Folders'; 
+$L['Update_Header'] = 'Edit shared folder `${0}`'; 
+$L['customValue_label'] = ''; 
+$L['ibay_label'] = 'Name'; 
+$L['reset-permissions_Header'] = 'Reset permissions on folder `${0}`'; 
+$L['reset-permissions_Message'] = 'Reset permissions to default value on files and sub-folder of shared folder `${0}`?'; 
+$L['reset-permissions_label'] = 'Reset permissions'; 
+$L['valid_platform,ibay-create,ibay-length,1'] = 'Name too long'; 
+$L['valid_platform,ibay-create,uniq-account,3'] = 'An account with the same name already exists'; 
--- root/usr/share/nethesis/NethServer/Language/it/NethServer_Module_SharedFolder.php
+++ root/usr/share/nethesis/NethServer/Language/it/NethServer_Module_SharedFolder.php
@@ -1,27 +1,23 @@
-<?php
-/**
- */
+<?php 
 
-$L['SharedFolder_Title'] = 'Cartelle condivise';
-$L['SharedFolder_Description'] = 'Condividi file e cartelle tra utenti e gruppi';
-
-$L['Create_Header'] = 'Crea una nuova cartella condivisa';
+$L['AclRead_label'] = 'Lettura'; 
+$L['AclWrite_label'] = 'Scrittura'; 
+$L['Acl_Title'] = 'ACL'; 
+$L['Authenticated users'] = 'Utenti autenticati'; 
+$L['BaseInfo_Title'] = 'Generali'; 
+$L['Create_Header'] = 'Crea una nuova cartella condivisa'; 
+$L['GroupAccess_label'] = 'Consenti scrittura al gruppo proprietario'; 
+$L['Key_label'] = 'Nome'; 
+$L['OtherAccess_label'] = 'Consenti lettura a tutti'; 
+$L['OwningGroup_label'] = 'Gruppo proprietario'; 
+$L['SharedFolder_Description'] = 'Condividi file e cartelle tra utenti e gruppi'; 
+$L['SharedFolder_Tags'] = 'cartelle directory condivise shared ibay'; 
+$L['SharedFolder_Title'] = 'Cartelle condivise'; 
 $L['Update_Header'] = 'Modifica la cartella condivisa `${0}`';
-
-$L['BaseInfo_Title'] = 'Generali';
-$L['Acl_Title'] = 'ACL';
-$L['ibay_label'] = 'Nome';
-$L['Key_label'] = 'Nome';
-$L['OwningGroup_label'] = 'Gruppo proprietario';
-$L['GroupAccess_label'] = 'Consenti scrittura al gruppo proprietario';
-$L['OtherAccess_label'] = 'Consenti lettura a tutti';
-$L['AclRead_label'] = 'Lettura';
-$L['AclWrite_label'] = 'Scrittura';
-$L['Authenticated users'] = 'Utenti autenticati';
-
-$L['valid_platform,ibay-create,uniq-account,3'] = 'C\'è già un account con questo nome';
-$L['valid_platform,ibay-create,ibay-length,1'] = 'Nome troppo lungo';
-
-$L['reset-permissions_label'] = 'Reimposta permessi';
-$L['reset-permissions_Header'] = 'Reimposta i permessi per la cartella `${0}`';
-$L['reset-permissions_Message'] = 'Reimposta i permessi al valore di default per file e sotto-cartelle della cartella condivisa `${0}`?';
+$L['customValue_label'] = ''; 
+$L['ibay_label'] = 'Nome'; 
+$L['reset-permissions_Header'] = 'Reimposta i permessi per la cartella `${0}`'; 
+$L['reset-permissions_Message'] = 'Reimposta i permessi al valore di default per file e sotto-cartelle della cartella condivisa `${0}`?'; 
+$L['reset-permissions_label'] = 'Reimposta permessi'; 
+$L['valid_platform,ibay-create,ibay-length,1'] = 'Nome troppo lungo'; 
+$L['valid_platform,ibay-create,uniq-account,3'] = 'C\'è già un account con questo nome'; 
